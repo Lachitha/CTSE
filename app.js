7704013fe0b27516718bb27df6b0ea12a9d204a9
Lachitha <it21019586@my.sliit.lk>
2024-04-16 22:31:42 +0530

    app updated

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+"use strict";
+
 const express = require("express");
+
+// Constants
+const PORT = 3000;
+const HOST = "0.0.0.0";
+
+// App
 const app = express();
-
 app.get("/", (req, res) => {
-	res.send("Hello, world!");
+	res.send("Hello World");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
